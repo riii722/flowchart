5efa77019ed02a25a0e81b465196845c3b3f266d
rii722 <ilovekikiandjiji@gmail.com>
2019-12-27 21:25:24 +0900

    問題を回答

---
+++ flowchart/src/lesson1/No2.java
@@ -0,0 +1,14 @@
+package lesson1;
+
+public class No2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Integer a = 8;
+		Integer b = 2;
+		Integer c = a-b;
+		c = c/b;
+
+	}
+
+}
---
+++ flowchart/src/lesson1/No3.java
@@ -0,0 +1,15 @@
+package lesson1;
+
+public class No3 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		Integer a =6;
+		Integer b = 2;
+		Integer c = a*b;
+		c = c+1;
+
+	}
+
+}
